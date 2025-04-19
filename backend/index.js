fcf8645d489718ad88022682bb104457448050bd
Guna-meda <meda.gunavathi25@gmail.com>
2025-04-19 17:41:42 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const fetch = require("node-fetch");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
